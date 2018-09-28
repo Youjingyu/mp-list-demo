c15a42090cb9aec9265eff2645a0c9ac4148e7ac
Youjingyu <735284268@qq.com>
2018-09-28 18:01:59 +0800

    update:

--- utils/recycling-list.js
+++ utils/recycling-list.js
@@ -3,24 +3,25 @@ const defaultOptions = {
   wx: null,
   height: 667,
   name: 'recycling-list',
-  countPerPage: 10,
+  // countPerPage: 10,
   onUpdate: noop
 }
 
 class RecyclingList {
   constructor (options) {
-    this.options = Object.assign(defaultOptions, options)
     try {
-      defaultOptions.height = this.options.wx.getSystemInfoSync().windowHeight
+      defaultOptions.height = options.wx.getSystemInfoSync().windowHeight
     } catch (err) {
       console.error('recycling-list get windowHeight error: ' + err.message)
     }
+    this.options = Object.assign(defaultOptions, options)
     this.query = this.options.wx.createSelectorQuery()
-    this.page = 0
+    // this.page = 0
     this.list = []
-    this.renderList = []
-    this.startRenderIndex = 0
-    this.endRenderIndex = undefined
+    // this.renderList = []
+    this.scrollTop = 0
+    // this.startRenderIndex = 0
+    // this.endRenderIndex = undefined
   }
   append (list) {
     this.list = this.list.concat(list.map((item) => {
@@ -33,30 +34,42 @@ class RecyclingList {
   }
   scroll (e) {
     const { scrollTop } = e.detail
-    const currentPage = Math.floor(scrollTop / this.options.height)
+    // const currentPage = Math.ceil(scrollTop / this.options.height)
+    // console.log(currentPage)
     // 页码变化
-    if (currentPage !== this.page) {
-      this.page = currentPage
-      this.update()
+    console.log('滑动距离：' + (scrollTop - this.scrollTop))
+    if (Math.abs(scrollTop - this.scrollTop) > this.options.height) {
+      // this.page = currentPage
+      setTimeout(() => {
+        this.scrollTop = scrollTop
+        this.update(scrollTop)
+      }, 300)
     }
   }
   update () {
     // 渲染前后5页
-    this.startRenderIndex = (this.page - 2) * this.options.countPerPage
-    this.endRenderIndex = (this.page + 2) * this.options.countPerPage
-    console.log('currentPage: ' + this.page)
+    // this.startRenderIndex = (this.page - 2) * this.options.countPerPage
+    // this.endRenderIndex = (this.page + 2) * this.options.countPerPage
+    // console.log('currentPage: ' + this.page)
+    let listHeight = 0
+    const renderRange = this.options.height * 2
     const tasks = []
     this.list.forEach((item, i) => {
-      const data = (i >= this.startRenderIndex && i <= this.endRenderIndex) ? item.data : 0
+      const distance = Math.abs(this.scrollTop - listHeight)
+      console.log('distance: ' + distance)
+      const data = distance < renderRange ? item.data : 0
       const id = this.options.name + '-' + i
+
       const task = new Promise((resolve) => {
         if (item.height !== undefined && item.height !== null) {
+          listHeight += item.height
           resolve({
             data,
             id,
             height: item.height + 'px'
           })
         } else {
+          console.log('data: ' + data)
           this.options.wx.createSelectorQuery().select('#' + id).fields({ size: true }, (res) => {
             item.height = res && res.height
             resolve({
